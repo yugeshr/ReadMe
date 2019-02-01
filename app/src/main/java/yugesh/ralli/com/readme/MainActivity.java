1a60888121d276f9c2dd8e06e06f6d42998bf59a
yugeshralli <yugeshr16@gmail.com>
2019-02-01 14:01:42 +0530

    Added tesseract ocr

--- app/src/main/java/yugesh/ralli/com/readme/MainActivity.java
+++ app/src/main/java/yugesh/ralli/com/readme/MainActivity.java
@@ -1,13 +1,204 @@
 package yugesh.ralli.com.readme;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_photo;
+    ImageView imageView;
+
+    private static final int REQUEST_TAKE_PHOTO = 100;
+    private final int PERMISSION_REQUEST_CAMERA = 1;
+    private final int PERMISSION_REQUEST_STORAGE = 2;
+
+    private String mCurrentPhotoPath;
+    private Bitmap mSelectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkDataFiles();
+        checkCameraPermission();
+
+        btn_photo = findViewById(R.id.photoButton);
+        imageView = findViewById(R.id.imageView);
+
+        btn_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    dispatchTakePictureIntent();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void checkDataFiles() {
+        File dir = new File(getApplicationContext().getFilesDir(), "tessdata");
+        dir.mkdir();
+        File oldFile = new File(dir, "eng.traineddata");
+        if (!oldFile.exists()){
+            AssetManager assetManager = getAssets();
+            try (InputStream input = assetManager.open("eng.traineddata")) {
+                File file = new File(dir, "eng.traineddata");
+                OutputStream output = new FileOutputStream(file);
+                try {
+                    byte[] buffer = new byte[4 * 1024]; // or other buffer size
+                    int read;
+
+                    while ((read = input.read(buffer)) != -1) {
+                        output.write(buffer, 0, read);
+                    }
+
+                    output.flush();
+                } finally {
+                    output.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Log.d("TAG", "Training data not available");
+        }
+    }
+
+
+        private void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
+        }
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(),"Permission Granted, Now you can access Camera",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Permission Denied, You cannot access Camera.",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case PERMISSION_REQUEST_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(),"Permission Granted, Now you can access Camera",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Permission Denied, You cannot access Camera.",Toast.LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    private void dispatchTakePictureIntent() throws IOException {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = createImageFile();
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                        "yugesh.ralli.com.readme.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        galleryAddPic();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Log.d("TAG",mCurrentPhotoPath);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            File imageFile = new File(mCurrentPhotoPath);
+            Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getPath());
+            mSelectedImage = imageBitmap;
+            imageView.setImageBitmap(imageBitmap);
+            runTextRecognition();
+        }
+    }
+
+    private void runTextRecognition() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TessBaseAPI tessTwo = new TessBaseAPI();
+                tessTwo.init(getApplicationContext().getFilesDir().toString(), "eng");
+                tessTwo.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
+                tessTwo.setImage(mSelectedImage);
+                String recognizedText = tessTwo.getUTF8Text();
+                Log.d("TAG",recognizedText);
+                tessTwo.end();
+            }
+        }).start();
     }
 }
