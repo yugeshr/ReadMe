b358f6beb45524cf576441ac253c0fac49140e9f
yugeshralli <yugeshr16@gmail.com>
2019-03-26 14:32:45 +0530

    Added Crop and TTS feature

--- app/src/main/java/yugesh/ralli/com/readme/MainActivity.java
+++ app/src/main/java/yugesh/ralli/com/readme/MainActivity.java
@@ -2,116 +2,93 @@ package yugesh.ralli.com.readme;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.googlecode.tesseract.android.TessBaseAPI;
-
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.languageid.FirebaseLanguageIdentification;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.google.firebase.ml.vision.text.RecognizedLanguage;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btn_photo;
-    ImageView imageView;
+    FloatingActionButton btn_photo;
+    TextView textView;
 
     private static final int REQUEST_TAKE_PHOTO = 100;
     private final int PERMISSION_REQUEST_CAMERA = 1;
     private final int PERMISSION_REQUEST_STORAGE = 2;
+    private final int TTS_CHECK_CODE = 3;
 
-    private String mCurrentPhotoPath;
+    private String mCurrentPhotoPath, resultText, docName;
     private Bitmap mSelectedImage;
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkDataFiles();
+        //checkDataFiles();
         checkCameraPermission();
+        //checkTTS
+        checkTTS();
 
-        btn_photo = findViewById(R.id.photoButton);
-        imageView = findViewById(R.id.imageView);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
+        }
 
-        btn_photo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    dispatchTakePictureIntent();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        btn_photo = findViewById(R.id.actionButton);
+        textView = findViewById(R.id.textView);
+
+        btn_photo.setOnClickListener(view -> {
+            cropImage();
         });
     }
 
-    private void checkDataFiles() {
-        File dir = new File(getApplicationContext().getFilesDir(), "tessdata");
-        dir.mkdir();
-        File oldFile = new File(dir, "eng.traineddata");
-        if (!oldFile.exists()){
-            AssetManager assetManager = getAssets();
-            try (InputStream input = assetManager.open("eng.traineddata")) {
-                File file = new File(dir, "eng.traineddata");
-                OutputStream output = new FileOutputStream(file);
-                try {
-                    byte[] buffer = new byte[4 * 1024]; // or other buffer size
-                    int read;
-
-                    while ((read = input.read(buffer)) != -1) {
-                        output.write(buffer, 0, read);
-                    }
-
-                    output.flush();
-                } finally {
-                    output.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }else {
-            Log.d("TAG", "Training data not available");
-        }
-    }
-
-
-        private void checkCameraPermission() {
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED){
-            // Permission is not granted
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
-        }
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED){
-            // Permission is not granted
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSION_REQUEST_STORAGE);
-        }
-
+    private void checkCameraPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
+                != PackageManager.PERMISSION_GRANTED){
             // Permission is not granted
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CAMERA);
@@ -123,16 +100,15 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case PERMISSION_REQUEST_CAMERA:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(getApplicationContext(),"Permission Granted, Now you can access Camera",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access Camera", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(),"Permission Denied, You cannot access Camera.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Permission Denied, You cannot access Camera.", Toast.LENGTH_SHORT).show();
                 }
-                break;
             case PERMISSION_REQUEST_STORAGE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    Toast.makeText(getApplicationContext(),"Permission Granted, Now you can access Camera",Toast.LENGTH_SHORT).show();
+                if (grantResults.length == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Permission Granted, Now you can access Storage", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(),"Permission Denied, You cannot access Camera.",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Permission Denied, Now you canot access Storage", Toast.LENGTH_SHORT).show();
                 }
         }
     }
@@ -155,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
+        docName = imageFileName;
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
@@ -172,33 +149,172 @@ public class MainActivity extends AppCompatActivity {
         Log.d("TAG",mCurrentPhotoPath);
         File f = new File(mCurrentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
+        cropImage();
         mediaScanIntent.setData(contentUri);
         this.sendBroadcast(mediaScanIntent);
     }
 
+    private void cropImage() {
+        // start picker to get image for cropping and then use the image in cropping activity
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            File imageFile = new File(mCurrentPhotoPath);
-            Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getPath());
-            mSelectedImage = imageBitmap;
-            imageView.setImageBitmap(imageBitmap);
-            runTextRecognition();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == TTS_CHECK_CODE){
+            if(resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS){
+                // success, create the TTS instance
+                textToSpeech = new TextToSpeech(this,this);
+            }else {
+                // missing data, install it
+                Intent installIntent = new Intent();
+                installIntent.setAction(
+                        TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installIntent);
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                File imageFile = new File(resultUri.getPath());
+                Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getPath());
+                mSelectedImage = imageBitmap; //rotateImageIfRequired(imageBitmap,imageFile);
+                recogniseText();
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+    }
+
+    private void recogniseText() {
+        FirebaseApp.initializeApp(this);
+        FirebaseVisionImage firebaseVisionImage = FirebaseVisionImage.fromBitmap(mSelectedImage);
+        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        // Task completed successfully
+// ...
+        Task<FirebaseVisionText> firebaseVisionTextTask =
+                firebaseVisionTextRecognizer.processImage(firebaseVisionImage)
+                        .addOnSuccessListener(this::extractTextFromResult)
+                        .addOnFailureListener(e -> {
+                                    // Task failed with an exception
+                                    // ...
+                                });
+    }
+
+    private void extractTextFromResult(FirebaseVisionText firebaseVisionText) {
+        resultText = firebaseVisionText.getText();
+        Log.d("TAG",resultText);
+        textView.setText(resultText);
+        saveTextAsFile("temp",resultText);
+        identifyLanguage(resultText);
+        for (FirebaseVisionText.TextBlock block: firebaseVisionText.getTextBlocks()) {
+            String blockText = block.getText();
+            Float blockConfidence = block.getConfidence();
+            List<RecognizedLanguage> blockLanguages = block.getRecognizedLanguages();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (FirebaseVisionText.Line line: block.getLines()) {
+                String lineText = line.getText();
+                Float lineConfidence = line.getConfidence();
+                List<RecognizedLanguage> lineLanguages = line.getRecognizedLanguages();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (FirebaseVisionText.Element element: line.getElements()) {
+                    String elementText = element.getText();
+                    Float elementConfidence = element.getConfidence();
+                    List<RecognizedLanguage> elementLanguages = element.getRecognizedLanguages();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                }
+            }
         }
     }
 
-    private void runTextRecognition() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                TessBaseAPI tessTwo = new TessBaseAPI();
-                tessTwo.init(getApplicationContext().getFilesDir().toString(), "eng");
-                tessTwo.setPageSegMode(TessBaseAPI.PageSegMode.PSM_SINGLE_LINE);
-                tessTwo.setImage(mSelectedImage);
-                String recognizedText = tessTwo.getUTF8Text();
-                Log.d("TAG",recognizedText);
-                tessTwo.end();
+    private void saveTextAsFile(String filename, String content){
+        String fileName = docName + ".txt";
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),fileName);
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(content.getBytes());
+            fileOutputStream.close();
+            Log.d("TAG","Saved!");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.d("TAG","File not found!");
+        } catch (IOException e){
+            e.printStackTrace();
+            Log.d("TAG","Error saving!");
+        }
+
+    }
+
+    private void identifyLanguage(String resultText) {
+        FirebaseLanguageIdentification languageIdentifier =
+                FirebaseNaturalLanguage.getInstance().getLanguageIdentification();
+        languageIdentifier.identifyLanguage(resultText)
+                .addOnSuccessListener(
+                        languageCode -> {
+                            if (languageCode != "und") {
+                                Log.i("TAG", "Language: " + languageCode);
+                            } else {
+                                Log.i("TAG", "Can't identify language.");
+                            }
+                        })
+                .addOnFailureListener(
+                        e -> {
+                            // Model couldn’t be loaded or other internal error.
+                            // ...
+                        });
+    }
+
+    private void checkTTS(){
+        Intent checkIntent = new Intent();
+        checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkIntent, TTS_CHECK_CODE);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            if (textToSpeech != null) {
+                int result = textToSpeech.setLanguage(Locale.US);
+                if (result == TextToSpeech.LANG_MISSING_DATA ||
+                        result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.d("TAG", "TTS language is not supported ");
+                } else {
+                    textToSpeech.setSpeechRate((float) 0.7);
+                    Log.d("TAG", "TTS is ready");
+                }
             }
-        }).start();
+        } else {
+            Log.d("TAG", "TTS initialization failed");
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_tts : textToSpeech.speak(resultText,TextToSpeech.QUEUE_ADD,null);
+            case R.id.action_stop : textToSpeech.stop();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
